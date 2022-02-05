1311d1798ed9f64d1b10444fb504621bcbed69cf
jeje898281 <ctkho0828@gmail.com>
2022-02-05 23:23:58 +0800

    createapp

--- booksgo/booksgo/urls.py
+++ booksgo/booksgo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booksearch.views import search_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', search_page),
 ]
